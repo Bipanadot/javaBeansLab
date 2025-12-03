00a868988a135ec196141fa8262567f89b8891e5
Bipanadot <tharubipana87@gmail.com>
2025-12-03 09:43:38 +0545

    Add files via upload

---
+++ mainClass.java
@@ -0,0 +1,44 @@
+package javaBeanLab2;
+
+import java.beans.PropertyVetoException;
+
+public class mainClass {
+public static void main(String[] args) { 
+try {
+System.out.println("=== Simple Bean Test ==="); 
+StudentBean s = new StudentBean(); 
+s.setName("Ram");
+s.setAge(21);
+System.out.println(s.getName() + " - " + s.getAge());
+System.out.println("\n=== Bound Property Test ==="); 
+BoundPersonBean p = new BoundPersonBean(); 
+p.addPropertyChangeListener(evt -> {
+System.out.println("Old: " + evt.getOldValue()); 
+System.out.println("New: " + evt.getNewValue());
+});
+p.setName("Bipana");
+p.setName("Shyam");
+System.out.println("\n=== Constrained Property Test ==="); 
+ConstrainedBean acc = new ConstrainedBean(); 
+acc.addVetoableChangeListener(evt -> {
+if ((double) evt.getNewValue() < 0)
+throw new PropertyVetoException("Negative balance not allowed!", evt);
+});
+try {
+acc.setBalance(-500);
+} catch (Exception e) {
+System.out.println("Veto: " + e.getMessage());
+}
+System.out.println("\n=== Custom Event Test ==="); 
+TemperatureSensorBean ts = new TemperatureSensorBean();
+ts.addTemperatureListener(e -> System.out.println("Temperature changed: " + e));
+ts.setTemperature(60);
+System.out.println("\n=== Persistence Test ==="); 
+BeanPersistence.save(s, "student.dat");
+StudentBean loaded = (StudentBean) BeanPersistence.load("student.dat"); 
+System.out.println("Loaded Name: " + loaded.getName());
+} catch (Exception e) { 
+e.printStackTrace();
+}
+}
+}
